b99e7a278b50d37844582427940916e691a86242
Fahd <jibreelfahd@gmail.com>
2024-06-20 22:08:11 +0100

    Added memo to Demo and Button components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import Button from "./components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler = () => {
+  console.log('App running');
+  const showParagraphHandler = useCallback(() => {
     setShowParagraph(prevParagraph => !prevParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is the new paragraph</p>}
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+      <DemoOutput show={false} />
+      <Button onClick={showParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
